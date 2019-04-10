cfc9420f3837dcf7bdface82f8614eab424284ea
Elie Liabeuf <eliabeuf@steamulo.com>
2019-04-10 12:50:45 +0200

    [IMPR] add support for tomcat7 app

---
+++ molecule/tomcat7/tests/test_default.py
@@ -0,0 +1,21 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_app_running_and_enabled(host):
+    app_service = host.service("test_tomcat7")
+    assert app_service.is_running
+    assert app_service.is_enabled
+
+
+def test_app_listening(host):
+    assert host.socket("tcp://0.0.0.0:8088").is_listening
+
+
+def test_app_response(host):
+    resp = host.run("curl localhost:8088/hello").stdout
+    assert "the Hello, World application" in resp

43316288c51bfbd8ae826b79393ef6c768bcffcd
Julien Basson <jbasson@steamulo.com>
2019-06-07 14:34:38 +0200

    [FEAT] play2 project type added to steamengine v2

---
+++ molecule/play/tests/test_default.py
@@ -0,0 +1,21 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_app_running_and_enabled(host):
+    app_service = host.service("play2_test")
+    assert app_service.is_running
+    assert app_service.is_enabled
+
+
+def test_app_listening(host):
+    assert host.socket("tcp://0.0.0.0:8085").is_listening
+
+
+def test_app_response(host):
+    resp = host.run("curl localhost:8085").stdout
+    assert "Your new application is ready" in resp
